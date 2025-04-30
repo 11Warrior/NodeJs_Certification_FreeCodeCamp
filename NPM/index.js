c64b03e0246cd0a18b1f66cc718b5af37a6fd380
Aayush <aayushthapa640@gmail.com>
2025-04-30 11:28:41 +0530

    update0

--- NPM/index.js
+++ NPM/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 
-app.listen(1100, () => {
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 1100');
 
 })
